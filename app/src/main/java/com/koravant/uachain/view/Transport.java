fc74b3074d1dc8c6457b3d43529a74892b156e9f
salim KORA GUERA <skora_guera@x-afs.com>
2022-02-07 23:45:26 +0100

    First commit

---
+++ app/src/main/java/com/koravant/uachain/view/Transport.java
@@ -0,0 +1,16 @@
+package com.koravant.uachain.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.koravant.uachain.R;
+
+public class Transport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transport);
+    }
+}
